bc10c8f09747122f22f10bfeb4997cebec1dee30
JavadKhanloo <khanloo.1414@gmail.com>
2024-02-21 12:11:37 +0330

    fcreate module

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'سامانه مدیریت بیمارستان',
+    'version': '15.0.1.0.0',
+    'author': 'جواد خانلو',
+    'sequence': -200,
+    'summary': 'سامانه مدیر بیمارستان',
+    'description': """سامانه مدیر بیمارستان""",
+    'depends': ['modern_theme'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
